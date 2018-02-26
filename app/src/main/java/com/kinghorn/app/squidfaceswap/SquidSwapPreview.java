fa65a71390d31d9d7c9c728b522b6502148ab70f
ddcjkinghorn <john.kinghorn@coxautoinc.com>
2018-02-26 13:33:51 -0500

    update

--- app/src/main/java/com/kinghorn/app/squidfaceswap/SquidSwapPreview.java
+++ app/src/main/java/com/kinghorn/app/squidfaceswap/SquidSwapPreview.java
@@ -1,10 +1,14 @@
 package com.kinghorn.app.squidfaceswap;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -32,8 +36,28 @@ public class SquidSwapPreview extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_squid_swap_start, menu);
+        getMenuInflater().inflate(R.menu.preview_menu, menu);
         return true;
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        switch(id){
+            case R.id.share:
+                Intent shar = new Intent();
+                shar.setAction(Intent.ACTION_SEND);
+                //Grab the URI of the temporary image and send it to next activity.
+                shar.putExtra(Intent.EXTRA_TEXT,"testing");
+                shar.setType("text/plain");
+                startActivity(shar);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
